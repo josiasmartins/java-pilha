a4d508916e624933d63646d3ead45d5a3dc441c0
Caitano <josiasmartinscaitano@gmail.com>
2023-01-15 19:43:47 -0300

    feat: pilha de execucao

---
+++ src/Console.java
@@ -0,0 +1,12 @@
+public class Console {
+
+    public static void logln(Object object) {
+        System.out.println(object);
+    }
+
+    public static void log(Object object) {
+        System.out.print(object);
+    }
+
+
+}

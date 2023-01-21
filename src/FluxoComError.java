9e10ecfd85f7273f2a223afb67d748485207f09c
Caitano <josiasmartinscaitano@gmail.com>
2023-01-21 15:22:37 -0300

    feat: checked e unchecked

--- src/FluxoComError.java
+++ src/FluxoComError.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComError {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -16,19 +16,15 @@ public class Fluxo {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MinhaExcecao    {
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao  {
-        System.out.println("Ini do metodo2");
-        Conta c = new Conta();
-        // lanca a exececao
-        // throw: só funciona com execeçoes;
-        throw new MinhaExcecao("deu muito errado");
-
-        //System.out.println("Fim do metodo2");
+    private static void metodo2() {
+        Console.logln("Chamando o metodo 2");
+        metodo2();
+        Console.logln("fim do metodo 2");
     }
 }
